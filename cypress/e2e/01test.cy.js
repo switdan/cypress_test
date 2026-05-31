7abd9275224f004f3358d62abf36ad9e4579b4f0
switdan <switala.daniel@gmail.com>
2026-05-31 12:18:41 +0200

    Initial commit, tests 1.0

---
+++ cypress/e2e/01test.cy.js
@@ -0,0 +1,48 @@
+/// <reference types="cypress" />
+Cypress.config().waitForAnimations = true;
+
+import 'cypress-mochawesome-reporter/register';
+import { parametersAccountManager, URLs } from "../fixtures/parameters.js";
+
+describe("Web form verification", () => {
+  it("should open web app", () => {
+    cy.visit(parametersAccountManager.formURL);
+  });
+
+  it("should check UI of main page of form", () => {
+    cy.verifyFormUI(
+      parametersAccountManager.role,
+      parametersAccountManager.subtitleMainPage,
+      parametersAccountManager.buttonText,
+      parametersAccountManager.linkText,
+    );
+  });
+
+  it("should register to app", () => {
+    cy.registerToApp(
+      parametersAccountManager.linkText,
+      parametersAccountManager.testedLogin,
+      parametersAccountManager.testedPassword,
+      parametersAccountManager.setUpAccountButton,
+      URLs.registerURL
+    );
+  });
+
+  it("should login to app", () => {
+    cy.loginToApp(
+      parametersAccountManager.testedLogin,
+      parametersAccountManager.testedPassword,
+      parametersAccountManager.buttonText,
+      URLs.loginURL
+    );
+    cy.verifyLoggedUser(
+      parametersAccountManager.testedLogin,
+      parametersAccountManager.logOutButton,
+      URLs.loggedURL
+    );
+  });
+
+  it("should log out", () => {
+    //  cy.logoutFromApp()
+  });
+});
